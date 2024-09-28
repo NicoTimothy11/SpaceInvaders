32057ce6e508a1eb2b4aba690297a8e24dcca9bf
NicoTimothy11 <nico.timothy319@gmail.com>
2024-09-28 21:19:51 +0800

    Newest Changes

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 int main()
 {
     Color black = {0, 0, 0, 255};
+    Color pink = {255, 192, 203, 255};
+    int offset = 50;
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "Space Invaders");
+    InitWindow(screenWidth + offset, screenHeight + 2 * offset, "Space Invaders");
     SetTargetFPS(60);
 
     Game game;
@@ -21,6 +23,7 @@ int main()
         game.Update();
         BeginDrawing();
         ClearBackground(black);
+        DrawRectangleRoundedLines({10, 10, 1310, 800}, 0.18f, 20, 2, pink);
         game.Draw();
         EndDrawing();
     }
